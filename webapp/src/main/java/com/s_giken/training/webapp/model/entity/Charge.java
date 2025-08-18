2d03b7acee776e640ce79b898995cb612a5109d8
yamaguchi-ryoka <yamaguchi-ryoka@host.docker.internal>
2025-08-18 16:18:27 +0900

    テキストボックスに不正な値が入力された際のバリデーションを実装

--- webapp/src/main/java/com/s_giken/training/webapp/model/entity/Charge.java
+++ webapp/src/main/java/com/s_giken/training/webapp/model/entity/Charge.java
@@ -5,8 +5,11 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -21,9 +24,12 @@ public class Charge {
 	@Nullable
 	private Long chargeId;
 
+	@Size(min=4, max=16, message="1文字から64文字で指定してください。")
 	@NotBlank
 	private String name;
 
+	@Min(value=1, message="正の整数を入力してください。")
+	@Digits(integer = 10, fraction = 0)
 	@NotNull
 	private BigDecimal amount;
 
